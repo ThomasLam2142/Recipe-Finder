c4c1168eb3e7048367a25154057dd268d76245c7
ThomasLam2142 <thomaslam2142@gmail.com>
2023-08-31 13:50:31 -0400

    able to fetch for included ingredients

--- recipe.js
+++ recipe.js
@@ -1,38 +1,48 @@
 // Replace 'YOUR_API_KEY' with your actual API key
-const apiKey = '2a9a9c88bd524e7aa76cc7ccd2293cbd';
-const baseUrl = 'https://api.spoonacular.com/';
 
-// Example endpoint: Get a random food joke
-const endpoint = 'food/jokes/random';
 
-// Construct the URL with the API key
-const apiUrl = `${baseUrl}${endpoint}?apiKey=${apiKey}`;
 
-let 
+function fetchRecipesByIngredients(ingredients) {
 
-// Make the GET request
-/*fetch(apiUrl)
-  .then((response) => {
-    // Check if the request was successful
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    // Do something with the data
-    console.log(data);
-  })
-  .catch((error) => {
-    // Handle errors
-    console.error('Error:', error);
-  });
-*/
+    const baseUrl = 'https://api.spoonacular.com/';
+    const apiKey = '2a9a9c88bd524e7aa76cc7ccd2293cbd';
+    // Join the ingredients into a comma-separated string
+    const ingredientsString = ingredients;
+
+    //const ingredientsString = ingredients.join(',');
+  
+    // Example endpoint: Recipe Search by Ingredients
+    const endpoint = `recipes/findByIngredients?ingredients=${ingredientsString}&apiKey=${apiKey}`;
+  
+    // Construct the URL with the API key
+    const apiUrl = `${baseUrl}${endpoint}`;
+  
+    // Make the GET request and return a Promise
+    return fetch(apiUrl)
+    .then((response) => {
+      // Check if the request was successful
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Log the recipe data to the console
+      console.log(data);
+      return data; // Return the data in case you want to use it elsewhere
+    })
+    .catch((error) => {
+      // Handle errors and log them
+      console.error('Error:', error);
+      throw error; // Re-throw the error for further handling if needed
+    });
+  }
 
 
 function findButton(){
     console.log("find button clicked");
     const includedIngredients = document.getElementById("includedElement").value;
     console.log(includedIngredients);
+    fetchRecipesByIngredients(includedIngredients);
 }
 
